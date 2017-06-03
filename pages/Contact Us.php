d208835fa6c368d4353fd13d06672476e1b57083
Trelon <legendaryxja@gmail.com>
2017-06-03 07:02:16 -0700

    Added contact us page

---
+++ pages/Contact Us.php
@@ -0,0 +1,82 @@
+<?php 
+include('../includes/header.php')
+?>
+
+   
+    <div id='home-container main-container' >
+       <!-- CoGraphix Code Goes Here -->  
+       
+    <h1>Contact Us</h1>
+        
+        
+<div class="container">
+  <h2>Have a question? Contact Us today</h2>
+  <form action="" method="post" >
+      
+      <div class="form-group">
+      <label for="name">Name:</label>
+      <input type="text" class="form-control" id="name" placeholder="Enter name" name="name" required>
+    </div>
+      
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" required>
+    </div>
+    
+    <div class="form-group">
+        <label for="message">Enter Message</label>
+        <textarea class="form-control" rows="5" id="massage" name="massage" required></textarea>
+    </div>
+      
+    <button type="submit" class="btn btn-default" name="submit">Submit</button>
+  </form>
+    
+    <?php
+    
+  
+        if(isset($_POST['name']))
+           {
+               $name=$_POST['name'];
+               $email=$_POST['email'];
+               $mess=$_POST['massage'];
+                $adminEmail="admin@website.com";
+            
+            
+				 $sub="Contact Us Response";
+            //message for the admin
+				 $body1=" This is an automated email. Please do not reply to this eamil.
+				 
+				$name has sent you the message: $mess 
+                 you can respond to them $email
+				 ";
+             //message for the client
+                $body2="This is an automated email. Please do not reply to this eamil.
+                Thank you for contacting Us. We will contact you as soon as possible.
+                
+                ";
+				 
+				 $headers = 'To: User <'.$name.'>'."\r\n";
+				 $headers .= "From: No-reply@websiteName.com\r\n";
+				$headers .= "Reply-To: No-reply@websiteName.com\r\n";
+				
+            mail($adminEmail,$sub,$body1,$headers);
+             mail($email,$sub,$body2,$headers);
+            
+				 
+   					echo '<div class="alert alert-success">
+  					<strong>Success!</strong> Message was sent.
+					</div>';
+            
+            
+           }
+        ?>
+    
+        </div>
+ 
+
+
+<?php
+
+//include('../includes/footer.php')
+
+?>
